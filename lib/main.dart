620549dbfed343d2f866943fb6054d4715a7f1df
chloemk <minsung.chloe.kim@gmail.com>
2022-07-04 12:14:52 +0900

    feat: add Column to Scaffold body

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,50 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      'What\'s your favourite color?',
+      'What\'s your favorite animal?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My first App'),
+        ),
+        body: Column(
+          children: [
+            const Text('The question!'),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white60, // background
+                onPrimary: Colors.black, // foreground
+              ),
+              onPressed: null,
+              child: const Text('Answer 1'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white60,
+                onPrimary: Colors.black,
+              ),
+              onPressed: null,
+              child: const Text('Answer 2'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white60,
+                onPrimary: Colors.black,
+              ),
+              onPressed: null,
+              child: const Text('Answer 3'),
+            ),
+          ],
         ),
       ),
     );

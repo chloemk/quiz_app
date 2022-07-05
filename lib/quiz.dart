7f616aef37621a5493dcfa1311214877a4f1ec24
chloemk <minsung.chloe.kim@gmail.com>
2022-07-05 12:19:09 +0900

    feat: add parameter in answerQuestion function

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -6,7 +6,7 @@ import 'question.dart';
 class Quiz extends StatelessWidget {
   final List<Map<String, Object>> questions;
   final int questionIndex;
-  final void Function() answerQuestion;
+  final void Function(int) answerQuestion;
 
   const Quiz({
     required this.questions,
@@ -19,8 +19,10 @@ class Quiz extends StatelessWidget {
     return Column(
       children: [
         Question(questions[questionIndex]['questionText'] as String),
-        ...(questions[questionIndex]['answers'] as List<String>)
-            .map((answer) => (Answer(answerQuestion, answer)))
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) => (Answer(
+                () => answerQuestion(answer['score'] as int),
+                answer['text'] as String)))
             .toList(),
       ],
     );
